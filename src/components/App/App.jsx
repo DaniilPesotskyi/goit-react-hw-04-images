402f2513732ded01ebc2747ae33d6cda996f76ed
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-05-23 20:41:11 +0300

    refactoring finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from '../Searchbar/Searchbar';
 import ImageGallery from '../ImageGallery/ImageGallery';
 
 const App = () => {
-    const [searchQuery, setSearchQuery] = useState()
+  const [searchQuery, setSearchQuery] = useState();
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [inLoad, setIsLoad] = useState(false);
 
-    const onSetSearchQuery = query => {
-        setSearchQuery(query)
+
+
+
+  useEffect(() => {
+    // FETCH IMAGES FUNCTION
+    const fetchImages = async (query, page) => {
+        try {
+            const response = await fetch(
+                `https://pixabay.com/api/?q=${query}&page=${page}&key=35032978-dd517b2e1f79f34a9f05731b1&image_type=photo&orientation=horizontal&per_page=12`
+            );
+            const images = await response.json();
+            return images.hits;
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    return (
-        <>
-            <Searchbar onSubmit={onSetSearchQuery} />
-            <ImageGallery query={searchQuery}/>
-            <ToastContainer autoClose={2500}/>
-        </>
-    );
-}
+    const getImages = async () => {
+      try {
+        setIsLoad(true);
+
+        const newImages = await fetchImages(searchQuery, page);
+        setImages(i => page === 1 ? newImages : [...i, ...newImages])
+        setIsLoad(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoad(false);
+      }
+    };
+
+    searchQuery && getImages();
+}, [page, searchQuery]);
+
+  const onLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  // SUBMIT FORM
+  const onSetSearchQuery = query => {
+    setPage(1)
+    setSearchQuery(query);
+  };
+
+  return (
+    <>
+      <Searchbar onSubmit={onSetSearchQuery} />
+      <ImageGallery items={images} inLoad={inLoad} onLoadMore={onLoadMore}/>
+      <ToastContainer autoClose={2500} />
+    </>
+  );
+};
 
 export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,92 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
 import Loader from '../Loader/Loader';
 import css from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  state = {
-      images: [],
-      page: 1,
-      inLoad: false
-  };
-
-  incrementPage = () => {
-      this.setState(prevState => ({
-          page: prevState.page + 1,
-      }));
-  };
-
-  addImages = newImages => {
-      this.setState(prevState => ({
-          images: [...prevState.images, ...newImages],
-      }));
-  };
-
-  fetchImages = async (query, page) => {
-      try {
-
-          this.setState({inLoad: true})
-          const response = await fetch(
-              `https://pixabay.com/api/?q=${query}&page=${page}&key=35032978-dd517b2e1f79f34a9f05731b1&image_type=photo&orientation=horizontal&per_page=12`
-          );
-          const images = await response.json();
-          this.setState({inLoad: false})
-          return images.hits;
-      } catch (error) {
-          this.setState({inLoad: false})
-          console.log(error);
-      }
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-      const prevQuery = prevProps.query;
-      const currentQuery = this.props.query;
-
-      if (prevQuery !== currentQuery) {
-          this.setState({ images: [], page: 1 });
-          this.incrementPage()
-          this.fetchImages(currentQuery, this.state.page).then(images => {
-              this.addImages(images);
-          });
-      }
-  }
-
-  onLoadMore = () => {
-      this.setState({inLoad: true})
-      this.incrementPage()
-
-      this.fetchImages(this.props.query, this.state.page).then(images => {
-          this.addImages(images);
-      });
-      this.setState({inLoad: false})
-  }
-
-  render() {
-
-      return (
-          <>
-              <ul className={css.galleryList}>
-                  {this.state.images.map(
-                      ({ id, webformatURL, largeImageURL }) => (
-                          <ImageGalleryItem
-                              key={id}
-                              imageURL={webformatURL}
-                              largeImageURL={largeImageURL}
-                          />
-                      )
-                  )}
-              </ul>
-              {this.state.images.length !== 0 && (
-                  <Button onLoadMore={this.onLoadMore} />
-              )}
-              {this.state.inLoad === true && (<Loader isOpen={this.state.inLoad}/>)}
-              {this.state.images.length === 0 && (
-                  <div className={css.noImagesText}>THERE`RE NO IMAGES YET</div>
-              )}
-          </>
-      );
-  }
+const ImageGallery = ({items, inLoad, onLoadMore}) => {
+  return (
+    <>
+        <ul className={css.galleryList}>
+            {items.map(
+                ({ id, webformatURL, largeImageURL }) => (
+                    <ImageGalleryItem
+                        key={id}
+                        imageURL={webformatURL}
+                        largeImageURL={largeImageURL}
+                    />
+                )
+            )}
+        </ul>
+        {items.length !== 0 && (
+            <Button onLoadMore={onLoadMore} />
+        )}
+        {inLoad === true && (<Loader isOpen={inLoad}/>)}
+        {items.length === 0 && (
+            <div className={css.noImagesText}>THERE`RE NO IMAGES YET</div>
+        )}
+    </>
+);
 }
 
 
